8038297d74928ccd7b9bb364da9169d8cdb1d8d3
zovelsanj <zovelsanj28@gmail.com>
2024-05-28 12:44:56 +0545

    Tlegend hashmap

--- codes/cpp/fit_distribution.cpp
+++ codes/cpp/fit_distribution.cpp
@@ -1,13 +1,6 @@
 #include "root_basics.hpp"
 #include<iostream>
 
-void get_params(TF1* fit)
-{
-    Double_t mean = fit->GetParameter(1);
-    Double_t sd = fit->GetParameter(2);
-    std::cout << "mean: " << mean << ", sd: " << sd << ", var: " << pow(sd, 2) << std::endl;
-}
-
 void fit_distribution()
 {
     TH1F *hist = new TH1F("hist", "Gaussian Distribution Fitting", 80, 0, 10);
@@ -22,11 +15,19 @@ void fit_distribution()
     
     hist->SetXTitle("Distribution");
     hist->SetYTitle("Entries");
+    hist->SetStats(0);
 
     TCanvas *c1 = new TCanvas();
     hist->Draw();
 
     TF1 *fit = new TF1("Gauss fit", "gaus", 0, 5);
     hist->Fit("Gauss fit", "Q"); //`R` => stricts the fit curve range, `Q` => quiet terminal info
-    get_params(fit);
+    basicFeatures *b = new basicFeatures(); 
+    b->get_params(fit);
+    
+    std::map<const char *, const TObject*> legend_hashmap;
+    legend_hashmap["histogram"] = hist;
+    legend_hashmap["gaussian fit"] = fit;
+
+    b->get_legends(legend_hashmap);
 }
--- codes/cpp/root_basics.hpp
+++ codes/cpp/root_basics.hpp
@@ -4,6 +4,7 @@
 #include<TRandom.h>
 #include<TGraph.h>
 #include<TGraphErrors.h>
+#include<TLegend.h>
 
 #pragma once
 class basicFeatures
@@ -11,8 +12,23 @@ class basicFeatures
     private:
         /* data */
     public:
-        void histogram();
-        void graph(int N);
-        void fit_distribution();
-        void get_params(TF1 *fit);
+        void get_params(TF1 *);
+        void get_legends(std::map<const char *, const TObject*>,  Option_t *, Double_t, Double_t, Double_t, Double_t);
 };
+
+void basicFeatures::get_params(TF1* fit)
+{
+    Double_t mean = fit->GetParameter(1);
+    Double_t sd = fit->GetParameter(2);
+    std::cout << "mean: " << mean << ", sd: " << sd << ", var: " << pow(sd, 2) << std::endl;
+}
+
+void basicFeatures::get_legends(std::map<const char *, const TObject*> legend_hashmap, Option_t *opt="l", Double_t xmin=0.7, Double_t ymin=0.7, Double_t xmax=0.9, Double_t ymax=0.9)
+{
+    TLegend *leg = new TLegend(xmin, ymin, xmax, ymax);
+    leg->Draw();
+    for(auto &it:legend_hashmap)
+    {
+        leg->AddEntry(it.second, it.first, opt);
+    }
+}
