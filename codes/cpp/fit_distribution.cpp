175012186247ae0916f1c150062f119ec2ec2226
zovelsanj <zovelsanj28@gmail.com>
2024-05-29 14:40:28 +0545

    stylize hist and fit

--- codes/cpp/fit_distribution.cpp
+++ codes/cpp/fit_distribution.cpp
@@ -5,6 +5,7 @@ void fit_distribution()
 {
     TH1F *hist = new TH1F("hist", "Gaussian Distribution Fitting", 80, 0, 10);
     TRandom *rn = new TRandom(314);
+    basicFeatures *b = new basicFeatures(); 
     Double_t val;
 
     for (int i = 0; i < 100; i++)
@@ -15,6 +16,11 @@ void fit_distribution()
     
     hist->SetXTitle("Distribution");
     hist->SetYTitle("Entries");
+    hist->GetXaxis()->SetTitleSize(0.03);
+    hist->GetYaxis()->SetTitleSize(0.03);
+    hist->GetXaxis()->SetLabelSize(0.03);
+    hist->GetYaxis()->SetLabelSize(0.03);
+
     hist->SetStats(0);
     hist->SetFillColor(kBlue-9);
 
@@ -22,10 +28,11 @@ void fit_distribution()
     hist->Draw();
 
     TF1 *fit = new TF1("Gauss fit", "gaus", 0, 5);
+    fit->SetLineStyle(2);
+    fit->SetLineColor(kRed);
     hist->Fit("Gauss fit", "Q");
-    basicFeatures *b = new basicFeatures(); 
+
     b->getParams(fit);
-    
     std::map<const char *, const TObject*> legend_hashmap;
     legend_hashmap["histogram"] = hist;
     legend_hashmap["gaussian fit"] = fit;
--- codes/cpp/root_basics.hpp
+++ codes/cpp/root_basics.hpp
@@ -23,7 +23,7 @@ void basicFeatures::getParams(TF1* fit)
     std::cout << "mean: " << mean << ", sd: " << sd << ", var: " << pow(sd, 2) << std::endl;
 }
 
-void basicFeatures::getLegends(std::map<const char *, const TObject*> legend_hashmap, Int_t border_size=4, Option_t *opt="l", Double_t xmin=0.7, Double_t ymin=0.7, Double_t xmax=0.9, Double_t ymax=0.9)
+void basicFeatures::getLegends(std::map<const char *, const TObject*> legend_hashmap, Int_t border_size=4, Option_t *opt="l", Double_t xmin=0.7, Double_t ymin=0.7, Double_t xmax=0.85, Double_t ymax=0.85)
 {
     TLegend *leg = new TLegend(xmin, ymin, xmax, ymax);
     leg->Draw();
@@ -33,3 +33,4 @@ void basicFeatures::getLegends(std::map<const char *, const TObject*> legend_has
         leg->AddEntry(it.second, it.first, opt);
     }
 }
+    
