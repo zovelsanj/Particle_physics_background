7adbe10ec295a2f076e1355ef371250b9df1d2cd
zovelsanj <zovelsanj28@gmail.com>
2024-05-30 09:41:44 +0545

    functions and error fit

--- codes/cpp/graph.cpp
+++ codes/cpp/graph.cpp
@@ -1,26 +1,25 @@
 #include "root_basics.hpp"
 
-void graph(int N = 25)
+void draw_graph(int N)
 {
-    TCanvas *cerr =  new TCanvas("Test", "Sample Error Bar");
-    cerr->SetGrid();
-    
-    TH1F *herr = cerr->DrawFrame(-0.4,0,10,100);
-    herr->SetXTitle("X title");
-    herr->SetYTitle("Y title");
     TRandom *rn = new TRandom();
- 
-    float x[N], y[N], xerr[N], yerr[N];
+    float x[N], y[N];
     for (int i = 0; i < N; i++)
     {
         x[i] = i*0.1;
-        y[i] = rn->Gaus(70.0, 10.0);;
+        y[i] = rn->Gaus(70.0, 10.0);
     }
 
     TGraph *g = new TGraph(N, x, y);  
     g->SetMarkerStyle(20);
     g->SetMarkerSize(1);
     g->Draw("ALP");
+}
+
+void draw_error_bar(int N, bool fit=false)
+{
+    float x[N], y[N], xerr[N], yerr[N];
+    TRandom *rn = new TRandom();
 
     for (int i = 0; i < N; i++)
     {
@@ -29,8 +28,26 @@ void graph(int N = 25)
         y[i] = rn->Gaus(50.0, 5.0);
         yerr[i] = y[i] * 0.08;
     }
-    TGraphErrors *gr2 = new TGraphErrors(10,x,y,xerr,yerr);
-    gr2->SetMarkerColor(kRed);
-    gr2->SetMarkerStyle(20);
-    gr2->Draw("LP");
+    TGraphErrors *gerr = new TGraphErrors(10, x, y, xerr, yerr);
+    gerr->SetMarkerColor(kRed);
+    gerr->SetMarkerStyle(20);
+    gerr->Draw("LP");
+
+    if (fit)
+    {
+        TF1 *f = new TF1("f", "curve fitting", 0, 5);
+        gerr->Fit("f");
+    }
+}
+
+void graph(int N = 25)
+{
+    TCanvas *cerr =  new TCanvas("Test", "Sample Error Bar");
+    cerr->SetGrid();
+    
+    TH1F *herr = cerr->DrawFrame(-0.4, 0, 10, 100);
+    herr->SetXTitle("X title");
+    herr->SetYTitle("Y title");
+    draw_graph(N);
+    draw_error_bar(N, true);
 }
