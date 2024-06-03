6db09d063f72520ae22799944fc7d9ee3f015561
zovelsanj <zovelsanj28@gmail.com>
2024-06-03 12:15:25 +0545

    2D surface plot

--- codes/cpp/root_basics/plots_2d.cpp
+++ codes/cpp/root_basics/plots_2d.cpp
@@ -30,46 +30,82 @@ void basic_plot_2d(int N, Int_t xbins = 100, Int_t ybins = 100, Int_t xlow = 0, 
     hist->Draw("colz");
 }
 
-void contour_2d(Int_t xlow = 0, Int_t xup = 100, Int_t ylow = 0, Int_t yup = 100)
+void contour_2d(TH2F *hist, Int_t xlow = 0, Int_t xup = 100, Int_t ylow = 0, Int_t yup = 100)
 {
     TCanvas *canvas = new TCanvas("canvas", "2D Contour", xlow, ylow, xup, yup);
     canvas->Divide(2, 2);
     canvas->SetFillColor(kEllipse);
 
-    TF2 *hist = new TF2("hist", "xygaus + xygaus(5) + xylandau(10)", xlow, xup, ylow, yup);
-    Double_t params[] = {130,-1.4,1.8,1.5,1, 150,2,0.5,-2,0.5, 3600,-2,0.7,-3,0.3};
-    hist->SetParameters(params);
-
-    TH2F *h2 = new TH2F("h2","Sample Contour Plot",20,-4,4,20,-4,4);
-    h2->FillRandom("hist");
-
     TPaveLabel *pl = new TPaveLabel();
 
     canvas->cd(1);
-    h2->Draw("contz");
+    hist->Draw("contz");
     gPad->SetGrid();
     pl->DrawPaveLabel(xlow, ylow, xup, yup, "contour1", "brNDC");
 
     canvas->cd(2);
-    h2->Draw("cont1");
+    hist->Draw("cont1");
     gPad->SetGrid();
     pl->DrawPaveLabel(xlow, ylow, xup, yup, "contour2", "brNDC");
 
     canvas->cd(3);
-    h2->Draw("cont2");
+    hist->Draw("cont2");
     gPad->SetGrid();
     pl->DrawPaveLabel(xlow, ylow, xup, yup, "contour3", "brNDC");
 
     canvas->cd(4);
-    h2->Draw("cont3");
+    hist->Draw("cont3");
     gPad->SetGrid();
     pl->DrawPaveLabel(xlow, ylow, xup, yup, "contour4", "brNDC");
 
     canvas->Update();
 }
 
+void surface_2d(TH2F *hist, Int_t xlow = 0, Int_t xup = 100, Int_t ylow = 0, Int_t yup = 100)
+{
+    TCanvas *canvas = new TCanvas("surfopt", "2D Contour", xlow, ylow, xup, yup);
+    canvas->Divide(2, 2);
+    canvas->SetFillColor(kEllipse);
+
+    TPaveLabel *pl = new TPaveLabel();
+
+    canvas->cd(1);
+    hist->Draw("surf1z");
+    gPad->SetGrid();
+    pl->DrawPaveLabel(xlow, ylow, xup, yup, "SURF1", "brNDC");
+
+    canvas->cd(2);
+    hist->Draw("surf2");
+    gPad->SetGrid();
+    pl->DrawPaveLabel(xlow, ylow, xup, yup, "SURF2", "brNDC");
+
+    canvas->cd(3);
+    hist->Draw("surf3");
+    gPad->SetGrid();
+    pl->DrawPaveLabel(xlow, ylow, xup, yup, "SURF3", "brNDC");
+
+    canvas->cd(4);
+    hist->Draw("surf4");
+    gPad->SetGrid();
+    pl->DrawPaveLabel(xlow, ylow, xup, yup, "SURF4", "brNDC");
+
+    canvas->Update();
+}
+
 void plots_2d()
 {
+    Int_t xbins = 50, ybins = 50;
+    Int_t xlow = -4,  xup = 4;
+    Int_t ylow = -4, yup = 4;
+
+    TF2 *hist = new TF2("hist", "xygaus + xygaus(5) + xylandau(10)", xlow, xup, ylow, yup);
+    Double_t params[] = {130,-1.4,1.8,1.5,1, 150,2,0.5,-2,0.5, 3600,-2,0.7,-3,0.3};
+    hist->SetParameters(params);
+
+    TH2F *h2 = new TH2F("h2","Sample Contour Plot", xbins, xlow, xup, ybins, ylow, yup);
+    h2->FillRandom("hist");
+
     basic_plot_2d(1e7);
-    contour_2d();
+    contour_2d(h2);
+    surface_2d(h2);
 }
